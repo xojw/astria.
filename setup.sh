52abc6a6652f6188a7abdcbcdbdcb74d3527a9ff
𓍼 <143974574+xojw@users.noreply.github.com>
2025-01-09 21:20:17 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -30,11 +30,7 @@ info "Step 2: Installing Node.js, npm, and Certbot..."
 sudo apt install -y nodejs npm certbot python3-certbot-nginx > /dev/null 2>&1
 separator
 
-info "Step 3: Installing Express..."
-npm install express > /dev/null 2>&1
-separator
-
-info "Step 4: Please enter your subdomain (e.g., subdomain.example.com):"
+info "Step 3: Please enter your subdomain (e.g., subdomain.example.com):"
 read -p "Subdomain: " SUBDOMAIN
 
 if [ -z "$SUBDOMAIN" ]; then
@@ -43,31 +39,31 @@ if [ -z "$SUBDOMAIN" ]; then
   exit 1
 fi
 
-info "Step 5: Requesting SSL certificate for $SUBDOMAIN..."
+info "Step 4: Requesting SSL certificate for $SUBDOMAIN..."
 sudo certbot --nginx -d $SUBDOMAIN
 separator
 
 success "SSL configuration complete for $SUBDOMAIN!"
 separator
 
-info "Step 6: Running conf.sh..."
+info "Step 5: Running conf.sh..."
 sudo bash /sh/conf.sh > /dev/null 2>&1
 separator
 
-info "Step 7: Running updates.sh..."
+info "Step 6: Running updates.sh..."
 sudo nohup bash /sh/updates.sh &> /updates.log &
 separator
 
-info "Step 8: Installing PM2..."
+info "Step 7: Installing PM2..."
 sudo npm install pm2 -g > /dev/null 2>&1
 separator
 
-info "Step 9: Configuring PM2 to start on boot..."
+info "Step 8: Configuring PM2 to start on boot..."
 pm2 startup
 separator
 
-info "Step 10: Starting the application with PM2..."
-pm2 start  index.mjs
+info "Step 9: Starting the application with PM2..."
+pm2 start index.mjs
 separator
 
 success "🎉 Congratulations! Your setup is complete, and your domain is now live with Ulrua! 🎉 You can now safely close this terminal."
